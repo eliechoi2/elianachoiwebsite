ffba304e50f97c7093122c0393f77cb0a0daaba2
eliechoi2 <102823232+eliechoi2@users.noreply.github.com>
2022-08-04 22:50:23 -0400

    a

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './index.css';
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home"
 import Portfolio from "./Portfolio"
+import Art from "./Art"
+import Contact from "./Contact"
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Home/>} />
             <Route exact path="/portfolio" element={<Portfolio />} />
+            <Route exact path="/art" element={<Art/>}/>
+            <Route exact path="/contact" element={<Contact/>}/>
           </Routes>
       </div>
   );
---
+++ src/Art.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function Art() {
+    return (
+        <div>
+            <Navbar/>
+            <div className="gallery">
+                <div className="row">
+                    <img className="artimages" alt="" src={require('./gallery/Assassin  Job 2.jpg')} />
+                    <img className="artimages" alt="" src={require('./gallery/Bennett  Job 2.jpg')} />
+                    <img className="artimages" alt="" src={require('./gallery/Blim  Job 2.jpg')} />
+                    <img className="artimages" alt="" src={require('./gallery/Chongyun  Job 2.jpg')} />
+                </div>
+                <div className="row">
+                    <img className="artimages" alt="" src={require('./gallery/Cloud  Job 2.jpg')} />
+                    <img className="artimages" alt="" src={require('./gallery/Diluc  Job 2.jpg')} />
+                    <img className="artimages" alt="" src={require('./gallery/Kaneki  Job 2.jpg')} />
+                    <img className="artimages" alt="" src={require('./gallery/Marth  Job 2.jpg')} />
+                </div>
+                <div className="row">
+                    <img className="artimages" alt="" src={require('./gallery/Music Job1.jpg')} />
+                    <img className="artimages" alt="" src={require('./gallery/Nymph   Job 6.jpg')} />
+                    <img className="artimages" alt="" src={require('./gallery/Paint   Job 6.jpg')} />
+                    <img className="artimages" alt="" src={require('./gallery/Razor 17 x 11 Job 4.jpg')} />
+                </div>
+                <div className="row">
+                    <img className="artimages" alt="" src={require('./gallery/Saber Job 2.jpg')} />
+                    <img className="artimages" alt="" src={require('./gallery/Sea Phantom  Job1.jpg')} />
+                    <img className="artimages" alt="" src={require('./gallery/Vampire Lord  Job 2.jpg')} />
+                    <img className="artimages" alt="" src={require('./gallery/VL  Job 5.jpg')} />
+                </div>
+                <div className="row">
+                    <img className="artimages" alt="" src={require('./gallery/Xiao  Job 2.jpg')} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Art
